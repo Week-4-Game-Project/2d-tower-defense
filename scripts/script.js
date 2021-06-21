9b1cb39ce81259c66912f0b0f10896d8f917766e
Mario P. Cardenas <15961362+Mario-paul@users.noreply.github.com>
2021-06-21 10:53:04 -0400

    add floatingMessage class

--- scripts/script.js
+++ scripts/script.js
@@ -151,6 +151,7 @@ class Defender {
     }
   }
 }
+// Defender summon on mouse click
 canvas.addEventListener("click", function () {
   const gridPositionX = mouse.x - (mouse.x % cellSize) + cellGap;
   const gridPositionY = mouse.y - (mouse.y % cellSize) + cellGap;
@@ -193,6 +194,33 @@ function handleDefenders() {
     }
   }
 }
+
+// FLOATING MESSAGES
+const floatingMessages = [];
+class floatingMessage {
+  constructor(value, x, y, size,color){
+this.value = value;
+this.x=x;
+this.y=y;
+this.size = size;
+this.lifeSpan = 0;
+this.color = color;
+this.opacity = 1;
+  }
+  update(){
+    this.y -= 0.3;
+    this.lifeSpan+= 1;
+    if(this.opacity > 0.01) this.opacity -= 0.01;
+  }
+  draw(){
+    ctx.globalAlpha = this.opacity;
+    ctx.fillStyle = this.color;
+    ctx.font = this.size + 'px Orbitron';
+    ctx.fillText(this.value,this.x,this.y);
+    ctx.globalAlpha = 1;
+  }
+}
+
 
 // ENEMIES
 class Enemy {
