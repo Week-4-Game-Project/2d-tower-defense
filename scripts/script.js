a8b16954a6c85f010e706d71b880c825ed4480c4
Vojislav Zaja <81269127+vojaso82@users.noreply.github.com>
2021-06-23 12:27:47 -0400

    test

--- scripts/script.js
+++ scripts/script.js
@@ -101,6 +101,84 @@ function handleFloatingMessags() {
     }
   }
 }
+const enemyTypes = [];
+const enemy1 = new Image();
+enemy1.src = "../images/enemy1.png";
+enemyTypes.push(enemy1);
+
+const enemy2 = new Image();
+enemy2.src = "../images/enemy2.png";
+enemyTypes.push(enemy2);
+
+const enemy3 = new Image();
+enemy3.src = "../images/enemy3.png";
+enemyTypes.push(enemy3);
+
+// Draws enemies array one time
+function handleEnemies() {
+  for (let i = 0; i < enemies.length; i++) {
+    enemies[i].update();
+    enemies[i].draw();
+    if (enemies[i].x < 0) {
+      gameOver = true;
+    }
+    if (enemies[i].health <= 0) {
+      let gainedResources = enemies[i].maxHealth / 10; // Resources gained from killing enemy
+      floatingMessages.push(
+        new floatingMessage(
+          "+" + gainedResources,
+          enemies[i].x,
+          enemies[i].y,
+          30,
+          "black"
+        )
+      );
+      floatingMessages.push(
+        new floatingMessage("+" + gainedResources, 250, 50, 30, "gold")
+      );
+      numberOfResources += gainedResources; // Gain resources based on enemy health
+      score += gainedResources; //Same for score
+      const findThisIndex = enemyPositions.indexOf(enemies[i].y);
+      enemyPositions.splice(findThisIndex, 1);
+      enemies.splice(i, 1);
+      i--;
+      console.log(enemyPositions);
+    }
+  }
+  if (frame % enemiesInterval === 0 && score < winningScore) {
+    let verticalPosition =
+      Math.floor(Math.random() * 5 + 1) * cellSize + cellGap;
+    enemies.push(new Enemy(verticalPosition, verticalPosition));
+    enemyPositions.push(verticalPosition);
+    if (enemiesInterval > 120) enemiesInterval -= 50;
+  }
+}
+
+function handleResources() {
+  if (frame % resourceFrequency === 0 && score < winningScore) {
+    resources.push(new Resource());
+  }
+  for (let i = 0; i < resources.length; i++) {
+    resources[i].draw();
+    if (resources[i] && mouse.x && mouse.y && collision(resources[i], mouse)) {
+      numberOfResources += resources[i].amount;
+      floatingMessages.push(
+        new floatingMessage(
+          "+" + resources[i].amount,
+          resources[i].x,
+          resources[i].y,
+          30,
+          "black"
+        )
+      );
+      floatingMessages.push(
+        new floatingMessage("+" + resources[i].amount, 250, 50, 30, "gold")
+      );
+      resources.splice(i, 1);
+      i--;
+    }
+  }
+}
 
 // UTILITIES
 // Draw game status on game bar (resources, defenders, etc)
@@ -147,7 +225,7 @@ function handleGameStatus() {
     ctx.fillStyle = "black";
     ctx.font = "90px Orbitron";
     ctx.fillText("GAME OVER", 135, 320);
-    sound.src = "./sounds/gameover.wav";
+    sound.src = "./sounds/funnySong.mp3";
     sound.play();
     // const button = document.getElementById("play-again"); // Play again button
     button.style.visibility = "visible";
